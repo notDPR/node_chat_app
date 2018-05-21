70867f48087ad14c06f21df18f946a4f306e67c7
Himanshu Santolia <himanshusantolia@live.com>
2018-05-21 13:30:38 +0530

    Add connect disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 const port = process.env.PORT || 3000 ;
 var app = express();
 
@@ -8,24 +10,24 @@ const publicPath = path.join(__dirname,'../public');
 // console.log(__dirname + '\..\public') ;
 // console.log(publicPath);
 
+var server = http.createServer(app);
+var io = socketIO(server);
+// http is itself used by express to create a server
+
 app.use(express.static(publicPath));
 
-// app.get('/',(req,res)=>{
-//   res.send(index.html);
-// });
+// lets you register an event listener
+io.on('connection' , (socket)=>{ // socket is similar to io(); in html file
+  console.log('New User connected');
 
-// ----------------------------------------------- //
-
-app.listen(port , ()=>{
-  console.log('ChatApp is live on port',port);
+  socket.on('disconnect',()=>{
+    console.log('User disconnected');
+  });
 });
 
 
+// ----------------------------------------------- //
 
-
-
-
-
-
-
-app.li
+server.listen(port , ()=>{
+  console.log('ChatApp is live on port',port);
+});
